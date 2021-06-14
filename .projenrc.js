b42895636d1beb71cbc11167b7fcadb7f36f3ead
Hal Blackburn <hwtb2@cam.ac.uk>
2021-06-14 18:29:05 +0100

    build: add tasks to build and push the tools docker images

--- .projenrc.js
+++ .projenrc.js
@@ -229,19 +229,62 @@ async function constructProject() {
     category: TaskCategory.TEST,
     exec: pythonProjects.map(proj => `cd ${proj.outdir} && poetry run pytest`).join(' && cd - && '),
   })
-  rootProject.addTask('build-release-docker-image-tilediiif.tools', {
-    category: TaskCategory.BUILD,
-    condition: `test "$(git rev-parse HEAD)" == "$(git rev-parse tags/tilediiif.tools-v${tilediiifTools.version}^{commit})"`,
-    exec: `
-    docker image build \\
-      --label "org.opencontainers.image.version=${tilediiifTools.version}" \\
-      --label "org.opencontainers.image.revision=$(git rev-parse HEAD)" \\
-      --tag camdl/tilediiif.tools:${tilediiifTools.version} \\
-      --build-arg TILEDIIIF_TOOLS_VERSION=${tilediiifTools.version} \\
-      --build-arg TILEDIIIF_CORE_VERSION=${tilediiifCore.version} \\
-      --target tilediiif.tools .
-  `,
+
+  const ensureReleaseable = rootProject.addTask('ensure-checkout-is-releaseable', {
+    category: TaskCategory.RELEASE,
+    description: 'fail with an error if the working copy is not a clean checkout of a release tag',
+    exec: `\
+      test "$(git rev-parse HEAD)" == "$(git rev-parse tags/tilediiif.tools-v${tilediiifTools.version}^{commit})" \\
+        && test "$(git status --porcelain)" == "" \\
+        || (echo "Error: the git checkout must be clean and tagged" 1>&2; exit 1)
+    `,
   });
+
+  const toolsDockerImageName = 'camdl/tilediiif.tools'
+  const toolsReleaseDockerImages = [
+    {name: 'slim', target: 'tilediiif.tools', tag: `${tilediiifTools.version}-slim`},
+    {name: 'default', target: 'tilediiif.tools-parallel', tag: tilediiifTools.version},
+  ]
+
+  const buildToolsReleaseDockerImageTasks = toolsReleaseDockerImages.map(({name, target, tag}) => {
+    const task = rootProject.addTask(`build-release-docker-images:tilediiif.tools:${name}`, {
+      category: TaskCategory.BUILD,
+      // only build if the tag doesn't already point to an image
+      condition: `! docker image inspect ${toolsDockerImageName}:${tag} > /dev/null 2>&1`,
+      exec: `\
+        docker image build \\
+          --label "org.opencontainers.image.version=${tag}" \\
+          --label "org.opencontainers.image.revision=$(git rev-parse HEAD)" \\
+          --tag ${toolsDockerImageName}:${tag} \\
+          --build-arg TILEDIIIF_TOOLS_VERSION=${tilediiifTools.version} \\
+          --build-arg TILEDIIIF_CORE_VERSION=${tilediiifCore.version} \\
+          --target "${target}" .
+      `,
+    });
+    task.prependSpawn(ensureReleaseable);
+    return task;
+  });
+
+  const buildToolsReleaseDockerImageTask = rootProject.addTask('build-release-docker-images:tilediiif.tools', {
+    category: TaskCategory.BUILD
+  });
+  buildToolsReleaseDockerImageTasks.forEach(task => buildToolsReleaseDockerImageTask.prependSpawn(task));
+
+  const pushToolsReleaseDockerImageTasks = toolsReleaseDockerImages.map(({name, target, tag}, i) => {
+    const task = rootProject.addTask(`push-release-docker-images:tilediiif.tools:${name}`, {
+      category: TaskCategory.RELEASE,
+    });
+    task.spawn(buildToolsReleaseDockerImageTasks[i]);
+    task.exec(`docker image push ${toolsDockerImageName}:${tag}`);
+    return task;
+  });
+  const pushToolsReleaseDockerImageTask = rootProject.addTask('push-release-docker-images:tilediiif.tools', {
+    category: TaskCategory.RELEASE
+  });
+  pushToolsReleaseDockerImageTasks.forEach(task => pushToolsReleaseDockerImageTask.prependSpawn(task));
+
+
+
   rootProject.addTask('format-python-code', {
     category: TaskCategory.MAINTAIN,
     cwd: __dirname,
