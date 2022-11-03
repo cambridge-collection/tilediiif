dff7f3017352796bb96809ef9d75e7fb09fa5f16
Hal Blackburn <hwtb2@cam.ac.uk>
2022-11-03 15:28:15 +0000

    feat: add initial trial IIIF Image req handler

--- tilediiif.cloudfront/src/index.ts
+++ tilediiif.cloudfront/src/index.ts
@@ -1,12 +1,28 @@
-import { foo } from "./foo";
+import {
+    canonicaliseRequestWithoutDimensions,
+    formatRequest,
+    parseRequest,
+} from "./iiif-image-2.1";
 
 export default function handler(
     event: AWSCloudFrontFunction.Event
-): AWSCloudFrontFunction.Request {
-    var match = /(\w+)-.*\.(\w+)/.exec(event.request.uri);
-    if (match) {
-        event.request.uri = `/${match[1]}.${match[2]}`;
+): AWSCloudFrontFunction.Request | AWSCloudFrontFunction.Response {
+    // The uri starts with a /, so ignore the first empty segment
+    const segments = event.request.uri.split("/").slice(1);
+    const req = parseRequest(segments);
+    if (req === null) {
+        const response: AWSCloudFrontFunction.Response = {
+            statusCode: 404,
+        };
+        return response;
     }
-    console.log(foo(3));
+    const canonicalReq = canonicaliseRequestWithoutDimensions(req);
+    if (canonicalReq !== req) {
+        const canonicalPath = `/${formatRequest(canonicalReq).join("/")}`;
+        console.log(
+            `Internal redirect from non-canonical: ${event.request.uri} to canonical: ${canonicalPath}`
+        );
+        event.request.uri = canonicalPath;
+    }
     return event.request;
 }
