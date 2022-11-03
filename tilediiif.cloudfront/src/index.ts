84dd332914c94b1db8e6ad1604bfbce5362d7d4b
Hal Blackburn <hwtb2@cam.ac.uk>
2022-11-03 16:56:21 +0000

    feat: implement basic IIIF image request handling
    We validate incoming requests by parsing them, do some basic
    canonicalisation (the most we can without making the image & tile size
    available in a context-free way, e.g. encoded in the identifier), and
    automatically 404 requests that are invalid or known to not resolve.

--- tilediiif.cloudfront/src/index.ts
+++ tilediiif.cloudfront/src/index.ts
@@ -1,6 +1,13 @@
 import {
     canonicaliseRequestWithoutDimensions,
+    formatRegion,
     formatRequest,
+    formatRotation,
+    formatSize,
+    ImageQuality,
+    ImageRegionType,
+    ImageRequestType,
+    ImageSizeType,
     parseRequest,
 } from "./iiif-image-2.1";
 
@@ -11,18 +18,40 @@ export default function handler(
     const segments = event.request.uri.split("/").slice(1);
     const req = parseRequest(segments);
     if (req === null) {
-        const response: AWSCloudFrontFunction.Response = {
-            statusCode: 404,
-        };
-        return response;
+        return response404();
     }
-    const canonicalReq = canonicaliseRequestWithoutDimensions(req);
-    if (canonicalReq !== req) {
-        const canonicalPath = `/${formatRequest(canonicalReq).join("/")}`;
-        console.log(
-            `Internal redirect from non-canonical: ${event.request.uri} to canonical: ${canonicalPath}`
-        );
-        event.request.uri = canonicalPath;
+    const cReq = canonicaliseRequestWithoutDimensions(req);
+    let resolvedPath: string;
+    if (
+        cReq.type === ImageRequestType.IMAGE &&
+        cReq.region.type === ImageRegionType.ABSOLUTE &&
+        cReq.size.type === ImageSizeType.ABSOLUTE &&
+        cReq.rotation.degrees === 0 &&
+        !cReq.rotation.mirrored &&
+        cReq.quality === ImageQuality.DEFAULT &&
+        cReq.format === "jpg"
+    ) {
+        // The key layout used for tiles in S3 is:
+        // {identifier}/{region}-{size.w},-{rotation}-{quality}.{format}
+        resolvedPath = `${cReq.identifier}/${formatRegion(
+            cReq.region
+        )}-${formatSize(cReq.size)}-${formatRotation(cReq.rotation)}-${
+            cReq.quality
+        }.${cReq.format}`;
+    } else if (cReq.type === ImageRequestType.IMAGE_INFO) {
+        resolvedPath = formatRequest(cReq).join("/");
+    } else {
+        return response404();
     }
+
+    event.request.uri = `/${resolvedPath}`;
     return event.request;
+}
+
+function response404(): AWSCloudFrontFunction.Response {
+    return {
+        statusCode: 404,
+        statusDescription: "Not Found",
+        headers: { "access-control-allow-origin": { value: "*" } },
+    };
 }
